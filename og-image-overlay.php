fcd0a1dacd7170edd01e1cfff7d72cbebcf6c8a9
Al-Mamun Talukder <admin@itsmereal.com>
2020-06-18 13:07:52 +0600

    version 1.1

--- og-image-overlay.php
+++ og-image-overlay.php
@@ -3,7 +3,7 @@
 Plugin Name: Open Graph Image Overlay
 Plugin URI: https://itsmereal.com/plugins/open-graph-image-overlay
 Description: Add automated image overlay on top of Open Graph images. This plugin extends the Open Graph features from Yoast SEO or Rank Math plugin.
-Version: 1.0
+Version: 1.1
 Author: Al-Mamun Talukder
 Author URI: https://itsmereal.com
 Requires at least: 4.3
