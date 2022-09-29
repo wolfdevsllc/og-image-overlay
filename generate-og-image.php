295c9e02600a2b633b6e2a9254a80bc620819320
Al-Mamun Talukder <admin@itsmereal.com>
2022-09-29 23:20:51 +0600

    Fix update

--- generate-og-image.php
+++ generate-og-image.php
@@ -2,8 +2,8 @@
 
 require_once('wp-load.php');
 
-if ( isset( $_GET ) && isset( $_GET['p'] ) ) {
-    $post_id = intval( esc_html( $_GET['p'] ) );
+$post_id = get_query_var('ogio');
+if ( !empty( $post_id ) ) {
     generate_og_image( $post_id );
 } else {
     wp_die( 'You can not access to the Open Graph Image without the post ID. Read the instructions properly and try again!' );
