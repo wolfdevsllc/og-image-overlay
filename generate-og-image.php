b60b73d7fea081e274f25b2fc99114405864f6d1
Al-Mamun Talukder <admin@itsmereal.com>
2022-09-29 23:35:09 +0600

    Fix issue

--- generate-og-image.php
+++ generate-og-image.php
@@ -6,5 +6,5 @@ $post_id = get_query_var('ogio');
 if ( !empty( $post_id ) ) {
     generate_og_image( $post_id );
 } else {
-    wp_die( 'You can not access to the Open Graph Image without the post ID. Read the instructions properly and try again!' );
+    wp_die( 'You can not access to the Open Graph Image without the post ID.' );
 }
